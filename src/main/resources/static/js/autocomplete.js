b85ea2842b631cd98d537d6fa609524e0cc8d2f6
camiiruiz13 <69661273+camiiruiz13@users.noreply.github.com>
2023-07-29 19:37:09 -0500

    Se arregla el autocompletar

--- src/main/resources/static/js/autocomplete.js
+++ src/main/resources/static/js/autocomplete.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     console.log("Hola mundo");
-    $("buscar_producto").autocomplete({
-        source : function (request, response) {
+    $("#buscar_producto").autocomplete({
 
+        source : function(request, response) {
             $.ajax({
                 url : "/factura/cargar-productos/" + request.term,
                 dataType : "json",
@@ -10,22 +10,20 @@ $(document).ready(function () {
                     term : request.term
                 },
 
-                success : function (data) {
-
-                    response($.map(data, function (item) {
-                        return{
+                success : function(data) {
+                    response($.map(data, function(item) {
+                        return {
                             value : item.id,
                             label : item.nombre,
                             precio : item.precio,
-                        }
+                        };
                     }));
-
                 },
             });
-
         },
 
-        select : function (event, ui) {
+        select : function(event, ui) {
+
             $("#buscar_producto").val(ui.item.label);
             return false;
         }
